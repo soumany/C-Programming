ba847204c0a72d2560f9164a2d1de0ee07bb8cd0
Many <soumany06@gmail.com>
2022-08-14 22:33:21 +0700

    commit

---
+++ Matrix.c
@@ -0,0 +1,49 @@
+#include<stdio.h> 
+ 
+int main() 
+{ 
+	//int a[3][3],b[3][3],c[3][3],i,j,k,sum; 
+ 
+    printf("\nEnter the matrix elements of A\n"); 
+    for(i=0;i<3;i++) 
+    { 
+        for(j=0;j<3;j++) 
+        { 
+            scanf("%d",&a[i][j]); 
+        } 
+        printf("\n"); 
+    } 
+    //printf("\nEnter the matrix elements of B\n"); 
+    for(i=0;i<3;i++) 
+    { 
+        for(j=0;j<3;j++) 
+        { 
+            scanf("%d",&b[i][j]); 
+        } 
+        printf("\n"); 
+    } 
+   // for(i=0;i<3;i++) 
+    { 
+        for(j=0;j<3;j++) 
+        { 
+            sum=0; 
+            for(k=0;k<3;k++) 
+            { 
+                sum=sum+a[i][k]*b[k][j]; 
+            } 
+            c[i][j]=sum; 
+        } 
+    } 
+    printf("\nProduct of two matrix is:\n\n"); 
+    for(i=0;i<3;i++) 
+    { 
+        for(j=0;j<3;j++) 
+        { 
+            printf("%d",c[i][j]); 
+            printf("\t"); 
+        } 
+        printf("\n\n"); 
+    } 
+ 
+	return 0; 
+} 
